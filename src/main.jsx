497b64ad9d6f4d8f786c31a9a093acb7b425dfa3
Max <maxi.gauthier112@gmail.com>
2026-01-13 17:34:55 +0100

    setup: router + main layout + pages scaffold

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Home from "./pages/Home";
+import Domaine from "./pages/Domaine";
+import Prestations from "./pages/Prestations";
+import Galerie from "./pages/Galerie";
+import Infos from "./pages/Infos";
+import Contact from "./pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "domaine", element: <Domaine /> },
+      { path: "prestations", element: <Prestations /> },
+      { path: "galerie", element: <Galerie /> },
+      { path: "infos", element: <Infos /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
