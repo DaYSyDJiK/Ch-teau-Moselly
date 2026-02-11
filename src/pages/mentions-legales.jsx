63bf099f549c53e597b363c37a553a3f0f5ae5f1
Max <maxi.gauthier112@gmail.com>
2026-02-11 17:56:38 +0100

    modification useSEO dans mentions legales

--- src/pages/mentions-legales.jsx
+++ src/pages/mentions-legales.jsx
@@ -1,12 +1,16 @@
 import useSEO from "../hooks/useSEO";
 
-  useSEO({
-    title: "Mentions légales | Château Moselly",
-    description: "Mentions légales du site Château Moselly.",
-    robots: "noindex, nofollow",
-  });
 
 export default function MentionsLegales() {
+
+
+    useSEO({
+        title: "Mentions légales | Château Moselly",
+        description: "Mentions légales du site Château Moselly.",
+        robots: "noindex, nofollow",
+    });
+
+
     return (
 
         <>
